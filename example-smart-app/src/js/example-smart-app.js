98de38309e4bf306d42912457de9ba6a10801ba1
shloknatarajan <shlok.natarajan@gmail.com>
2020-12-06 21:20:25 -0800

    from gatech github

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -17,7 +17,9 @@
                       code: {
                         $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
                               'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4',
+                              'http://loinc.org|3141-9', 'http://loinc.org|29463-7'
+                              ]
                       }
                     }
                   });
@@ -41,6 +43,8 @@
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
+          var weight = byCodes('29463-7');
+
 
           var p = defaultPatient();
           p.birthdate = patient.birthDate;
@@ -48,6 +52,7 @@
           p.fname = fname;
           p.lname = lname;
           p.height = getQuantityValueAndUnit(height[0]);
+          
 
           if (typeof systolicbp != 'undefined')  {
             p.systolicbp = systolicbp;
@@ -59,6 +64,7 @@
 
           p.hdl = getQuantityValueAndUnit(hdl[0]);
           p.ldl = getQuantityValueAndUnit(ldl[0]);
+          p.weight = getQuantityValueAndUnit(weight[0]);
 
           ret.resolve(p);
         });
@@ -83,6 +89,7 @@
       diastolicbp: {value: ''},
       ldl: {value: ''},
       hdl: {value: ''},
+      weight: {value: ''}
     };
   }
 
@@ -126,6 +133,7 @@
     $('#diastolicbp').html(p.diastolicbp);
     $('#ldl').html(p.ldl);
     $('#hdl').html(p.hdl);
+    $('#weight').html(p.weight);
   };
 
 })(window);
